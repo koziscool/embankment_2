ec2b0b3a60e92568336eafa5b3760938601fc327
koziscool <kozjob@yahoo.com>
2016-09-05 17:03:14 -0500

    more

---
+++ e6.py
@@ -0,0 +1,12 @@
+
+
+def e6():
+    end_range = 100
+    sum_squares = sum( i*i for i in xrange(1, end_range+1 ))
+    square_sum = pow( sum(xrange(1, end_range + 1)), 2)
+
+    return  square_sum - sum_squares
+
+if __name__ == "__main__":
+    print e6()
+
