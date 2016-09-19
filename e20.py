208fd1953bd70e36c2dc5125fac2cebc115fa4cb
koziscool <kozjob@yahoo.com>
2016-09-19 02:08:59 -0500

    more

---
+++ e20.py
@@ -0,0 +1,18 @@
+
+import time
+
+def e20():
+    mult = lambda  x, y: x*y
+    fac_100  = reduce( mult, xrange(1, 100), 1 )
+    current_digit_sum = 0
+    for c in str( fac_100 ):
+        current_digit_sum += int(c)
+    return current_digit_sum 
+
+if __name__ == "__main__":
+    start = time.time()
+    # print
+    print "Euler 20 solution is:",  e20()
+    end = time.time()
+    print "elapsed time is: %.4f milliseconds" % (1000 * (end - start)),
+    print
---
+++ e48.py
@@ -0,0 +1,22 @@
+
+import time
+
+def e48():
+    current_sum, end_range = 0, 1000
+    for i in xrange(1, end_range + 1 ):
+        current_addend = i
+        for _ in xrange(1, i ):
+            current_addend *= i
+        # print current_addend
+        current_sum +=  current_addend % (10 ** 10)
+
+    return current_sum % (10 ** 10)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    # print
+    print "Euler 48 solution is:",  e48()
+    end = time.time()
+    print "elapsed time is: %.4f milliseconds" % (1000 * (end - start)),
+    print
