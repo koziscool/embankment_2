9bae9890de09fe55817926043813de3b8e37da00
koziscool <kozjob@yahoo.com>
2016-09-21 16:14:00 -0500

    more

--- e12.py
+++ e12.py
@@ -42,30 +42,35 @@ def e12():
                         totients[j] *= i
                         k /= i
 
+    def get_factors( n ):
+        prime_product = reduce( operator.mul, prime_divisors[n], 1)
+        reduce_quotient = n / prime_product
+        factors_dict = { p:1 for p in prime_divisors[n] }
+        while reduce_quotient != 1:
+            prime_product = reduce( operator.mul, prime_divisors[reduce_quotient], 1)
+            for p in prime_divisors[reduce_quotient]:
+                factors_dict[p] += 1
+            reduce_quotient /= prime_product
+
+        return factors_dict
+
     primes_and_totients( begin_cursor, end_cursor )
 
     triangle_cursor = 1
     while True:
         triangle = triangle_cursor * (triangle_cursor + 1) / 2
-        if triangle > end_cursor:
+
+        if triangle_cursor + 1 > end_cursor:
             begin_cursor, end_cursor = end_cursor + 1, 2* end_cursor
             primes_and_totients( begin_cursor, end_cursor )
 
-        prime_product = reduce( operator.mul, prime_divisors[triangle], 1)
-        triangle_quotient = triangle / prime_product
-        factors_dict = { p:1 for p in prime_divisors[triangle]}
-        # print triangle, prime_divisors[triangle], prime_product, factors_dict
-        while triangle_quotient != 1:
-            prime_product = reduce( operator.mul, prime_divisors[triangle_quotient], 1)
-            for p in prime_divisors[triangle_quotient]:
-                factors_dict[p] += 1
+        factors_1 = get_factors( triangle_cursor )
+        factors_2 = get_factors( triangle_cursor + 1)
+        combined_factors = dict(factors_1, **factors_2)
+        combined_factors[2] -=1
+        num_factors = reduce( operator.mul, map( lambda n: n+1, combined_factors.values() ), 1)
 
-            # print triangle, triangle_quotient, prime_product
-            triangle_quotient /= prime_product
-        # print triangle, prime_divisors[triangle], prime_product, factors_dict
-        num_factors = reduce( operator.mul, map( lambda n: n+1, factors_dict.values() ), 1)
-        print triangle, prime_divisors[triangle], factors_dict, num_factors
-        if num_factors > 250:
+        if num_factors > 500:
             break
 
         triangle_cursor += 1
